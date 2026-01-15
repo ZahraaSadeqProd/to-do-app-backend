b7a4734f10fd92a73bb57a56bf41cb31c8c85058
Zahraa <zkarem12@gmail.com>
2026-01-14 21:45:24 -0700

    fixing error for using google stylesheet using helmet

--- src/app.js
+++ src/app.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
 
 const authRoutes = require('./routes/auth.routes');
 const todoRoutes = require('./routes/todo.routes');
 
+// Security headers with CSP that allows Google Fonts
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", 'https://fonts.googleapis.com'],
+      fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+      scriptSrc: ["'self'"],
+      imgSrc: ["'self'", 'data:', 'https:']
+    }
+  })
+);
+
 app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRoutes);
-
-app.get('/health', (req, res) => {
-  res.json({ status: 'OK' });
-});
-
 app.use('/todos', todoRoutes);
-
 
 module.exports = app;
